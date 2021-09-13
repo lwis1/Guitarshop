877537f2f7ba6e0975d5b844d1355a261f1f1260
lwis1 <82687553+lwis1@users.noreply.github.com>
2021-09-13 14:23:17 +0200

    Order place added p w f

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,39 +1,40 @@
-// import asyncHandler from 'express-async-handler'
-// import Order from '../models/orderModel.js'
+import asyncHandler from 'express-async-handler'
+import Order from '../models/orderModel.js'
 
-// // @desc    Create new order
-// // @route   POST /api/orders
-// // @access  Private
-// const addOrderItems = asyncHandler(async (req, res) => {
-//   const {
-//     orderItems,
-//     shippingAddress,
-//     paymentMethod,
-//     itemsPrice,
-//     taxPrice,
-//     shippingPrice,
-//     totalPrice,
-//   } = req.body
+// @desc    Create new order
+// @route   POST /api/orders
+// @access  Private
+const addOrderItems = asyncHandler(async (req, res) => {
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body
 
-//   if (orderItems && orderItems.length === 0) {
-//     res.status(400)
-//     throw new Error('No order items')
-//     return
-//   } else {
-//     const order = new Order({
-//       orderItems,
-//       user: req.user._id,
-//       shippingAddress,
-//       paymentMethod,
-//       itemsPrice,
-//       taxPrice,
-//       shippingPrice,
-//       totalPrice,
-//     })
+  if (orderItems && orderItems.length === 0) {
+    res.status(400)
+    throw new Error('No order items')
+    return
+  } else {
+    const order = new Order({
+      orderItems,
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    })
 
-//     const createdOrder = await order.save(addOrderItems)
-//     res.status(201).json(createdOrder)
-//   }
-// })
+    const createdOrder = await order.save()
+    res.status(201).json(createdOrder)
+  }
+})
 
 
+export {addOrderItems}
--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import {Row, Col, ListGroup, Image, Card,Button } from 'react-bootstrap'
 import CheckoutSteps from '../components/CheckoutSteps'
-import {useSelector} from 'react-redux'
+import {useSelector, useDispatch} from 'react-redux'
 import Message from '../components/Message'
 import { Link } from 'react-router-dom'
+import { createOrder } from '../actions/orderActions'
 
 
-const PlaceOrderScreen = () => {
+const PlaceOrderScreen = ({history}) => {
+    const dispatch = useDispatch()
     const cart = useSelector(state=> state.cart)
     //   Calculate prices
     const addDecimals = (num) => {
@@ -22,8 +24,27 @@ const PlaceOrderScreen = () => {
       Number(cart.shippingPrice) +
       Number(cart.taxPrice)
     ).toFixed(2)
+
+    const orderCreate = useSelector(state => state.orderCreate)
+    const { order, success, error} = orderCreate
+
+    useEffect(() => {
+      if(success) {
+        history.push(`/orders/${order._id}`)
+      }
+      // eslint-disable-next-line
+    },[history, success])
+
     const placeOrderHandler = () => {
-      console.log('order')
+      dispatch(createOrder({
+        orderItems: cart.cartItems,
+        shippingAddress: cart.shippingAddress,
+        paymentMethod: cart.paymentMethod,
+        itemsPrice: cart.itemsPrice,
+        shippingPrice: cart.shippingPrice,
+        taxPrice: cart.taxPrice,
+        totalPrice: cart.totalPrice,
+      }))
     }
     return (
     <>
@@ -109,9 +130,9 @@ const PlaceOrderScreen = () => {
                   <Col>${cart.totalPrice}</Col>
                 </Row>
               </ListGroup.Item>
-              {/* <ListGroup.Item>
+              <ListGroup.Item>
                 {error && <Message variant='danger'>{error}</Message>}
-              </ListGroup.Item> */}
+              </ListGroup.Item>
               <ListGroup.Item>
                 <Button
                   type='button'
