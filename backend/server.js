b4c3ab69244ec3c0e8ecc0569ae9e49156025bae
lwis1 <82687553+lwis1@users.noreply.github.com>
2021-10-07 19:13:52 +0200

    prepare for deploy

--- backend/server.js
+++ backend/server.js
@@ -22,10 +22,6 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(express.json())
 
-app.get('/',(req, res)=>{
-    res.send('API is runnig....')
-})
-
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoute)
 app.use('/api/orders', orderRoutes)
@@ -35,6 +31,15 @@ app.get('/api/config/paypal', (req,res) => res.send(process.env.PAYPAL_CLIENT_ID
 
 const __dirname =path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend','build','index.html')))
+} else {
+    app.get('/',(req, res)=>{
+        res.send('API is runnig....')
+    })
+}
 
 app.use(notFound)
 
